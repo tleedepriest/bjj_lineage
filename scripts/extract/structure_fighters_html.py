9cd990050418d4ad3190a793681d43bb79e6a110
Travis <tleedepriest@gmail.com>
2021-09-01 15:05:40 -0400

    removed spaces from filenames

--- scripts/extract/structure_fighters_html.py
+++ scripts/extract/structure_fighters_html.py
@@ -2,11 +2,30 @@
 This scripts reads html files and extracts data from the files
 """
 import sys
-from collections import OrderedDict
+import logging
 from pathlib import Path
+
+# from collections import OrderedDict
+
 import pandas as pd
+
+# import html2text
+
+import sqlite3
 from bs4 import BeautifulSoup
-import html2text
+
+def create_db():
+    """
+    """
+    con = sqlite3.connect('generated_data/bjj_heros.db')
+    con.execute('IF personal_info EXISTS DROP TABLE personal_info;')
+    con.execute("""CREATE TABLE personal_info 
+            (file_name PRIMARY KEY, 
+             full_name TEXT, 
+             nickname TEXT, 
+             lineage TEXT);""")
+    return con
+
 
 def get_full_name(soup_tag_name, soup_tag_element):
     """
@@ -28,25 +47,33 @@ def main(html_dir):
     -------------
     None
     """
-    fighters_df = pd.DataFrame(
-            {"introduction:": [], 
-             "full_name:": [],
-             "nickname:": [],
-             "lineage": []}
-            )
+    #fighters_df = pd.DataFrame(
+    #        {"introduction:": [], 
+    #         "full_name:": [],
+    #         "nickname:": [],
+    #         "lineage": []}
+    #        )
             
-    h = html2text.HTML2Text()
-    #h.ignore_links = True
+    # h = html2text.HTML2Text()
+    # h.ignore_links = True
+    
+    logging.basicConfig(
+            format='%(message)s', 
+            filename="logs/structure_fighter_html.log",
+            filemode='w', 
+            level=logging.DEBUG)
 
+    con = create_db()
     htmls = [x for x in Path(html_dir).glob('**/*') if x.is_file()]
     for html in htmls:
+        values = {}
+        values["file_name"] = Path(html).stem
         with open(html, 'r') as fh:
             contents = fh.read()
             #print(h.handle(contents))
             soup = BeautifulSoup(contents, features="lxml")
-            intro = soup.find('p')
-            next_sib = intro.next_sibling
-            
+            next_sib = soup.find('p')
+
             while True:
                 try:
                     next_sib = next_sib.next_sibling
@@ -57,21 +84,44 @@ def main(html_dir):
                     name = next_sib.name
                 except AttributeError:
                     name = ""
-
+                print(next_sib)
+                print(name)
                 if name == "p":
-                    # indicates bolded headers in webpage
                     text = next_sib.text
                     if "Full Name:" in text:
                         full_name = text.replace("Full Name: ", "")
+                        values["full_name"] = full_name
                     elif "Nickname:" in text:
                         nickname = text.replace("Nickname: ", "")
+                        values["nickname"] = nickname
+                    # need to use regex to fill in the number here
+                    # and then add that to the dictionary.
                     elif "Lineage:" in text:
                         lineage = text.replace("Lineage: ", "")
+                        values["lineage"] = lineage
                     elif "Main Achievements:" in text:
-                        print(next_sib.next_sibling)
-                        print(next_sib.next_sibling.next_sibling)
+                        pass
+                        #print(next_sib.next_sibling)
+                        #print(next_sib.next_sibling.next_sibling)
+        if not values:
+            logging.info(f"""There is no nickname, full_name, or lineage\n
+                    in this file: {html}""")
 
-                
+        elif len(values.keys())!=3:
+            logging.info(f"""This file: {html} \n
+                             contains dict without one or more of 
+                             nickname, full_name, or lineage...\n
+                             The keys are: \n
+                             {values.keys()}\n
+                             The values are:\n 
+                             {values.values()}""")
+        else:
+            con.execute("""INSERT INTO personal_info 
+                           (file_name, full_name, nickname, lineage)
+                           VALUES 
+                           (:file_name, :full_name, :nickname, :lineage);""", 
+                           values)
+
 if __name__ == "__main__":
     main(sys.argv[1])
 
