d649e9bf05e31a29963914eccb5cf09b5d2ff1c9
Travis <tleedepriest@gmail.com>
2021-09-02 06:16:58 -0400

    added simple default dict to count words and export to csv

--- scripts/extract/simple_txt_search.py
+++ scripts/extract/simple_txt_search.py
@@ -3,7 +3,8 @@ This script will perform a simple text search and create visualization to add
 in the extraction of key fields from the text.
 """
 import sys
-
+import pandas as pd
+from collections import defaultdict
 from pathlib import Path
 utils_abs_dir_path = Path(
         Path(__file__).parent.resolve().parent) / Path('utils')
@@ -11,7 +12,7 @@ sys.path.insert(0, str(utils_abs_dir_path))
 
 from file_utils import get_file_list, get_path_txt
 
-def main(txt_dir):
+def main(txt_dir, out_csv_path):
     """
     txt_dir: str
         the path to the directory you want to search
@@ -19,18 +20,37 @@ def main(txt_dir):
     out_csv_path: str
         the path of the csv file to produce
     """
-    words = ["Lineage 1:", "Lineage:", "Lineage :", "Linage:"]
+    # include common word for sanity check
+    # and to make sure same as number of files
+    words_to_count = [
+            "Lineage 1:", 
+            "Lineage:", 
+            "Lineage :", 
+            "Linage:",
+            "Full Name:",
+            "Nickname:",
+            "Full Name :",
+            "Nickname :",
+            "and",
+            "Lineage 2:",
+            "Lineage 3:",
+            "Lineage 4:"]
+
     txt_files = get_file_list(txt_dir)
-    len(txt_files)
-    count = 0
-    for txt_file in txt_files:
-        contents = get_path_txt(txt_file)
-        #if words[0] not in contents and words[1] not in contents and words[2] not in contents:
-        if words[3] in contents:
-            count+=1
-            print(txt_file)
-    print(count)
+    tally_occurence = defaultdict(int)
     print(len(txt_files))
+    for txt_file in txt_files:
+        print(txt_file)
+        contents = get_path_txt(txt_file)
+        for word in words_to_count:
+            if word in contents:
+                tally_occurence[word]+=1
+            else:
+                tally_occurence[word]+=0
+    print(tally_occurence)
+    df = pd.DataFrame(tally_occurence.items(), 
+            columns=["word", "count_across_corpus"])
+    df.to_csv(out_csv_path, index=False)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
