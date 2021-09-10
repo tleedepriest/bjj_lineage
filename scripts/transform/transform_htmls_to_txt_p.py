b240da3ac3cfddbda558fb8535d923f2fd8f18e1
Travis <tleedepriest@gmail.com>
2021-09-10 05:28:55 -0400

    added ability for script to parse out the main achievements sections of each html file

--- scripts/transform/transform_htmls_to_txt_p.py
+++ scripts/transform/transform_htmls_to_txt_p.py
@@ -4,43 +4,88 @@ This scripts reads html files and extracts data from the files
 import sys
 from pathlib import Path
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
 
-def get_p_txt(html):
+def get_soup(html):
+    """
+    Parameters
+    --------------
+    html: Path()
+        path object to html file
+    open html and convert to soup
+    """
+    with open(html, 'r') as fh:
+        contents = fh.read()
+        soup = BeautifulSoup(contents, features="lxml")
+    return soup
+
+def get_descendants_of_tag_txt(html, tag='ul'):
+    """
+    Parameters
+    --------------
+    html: Path()
+        the path to the html file
+    """
+    tag_txt = []
+    soup = get_soup(html)
+    # The ul element under Main Achievements has no class tag
+    all_ul = soup.find_all(tag, attrs={'class': None})
+
+    # The first two ul tags are ads
+    for ul in all_ul[2:]:
+        # we really want the <li> tags, as those are the acheivements
+        for children in ul.children:
+            
+            # this will raise attribute error
+            if isinstance(children, NavigableString):
+                continue
+            if isinstance(children, Tag):
+                print(children.text)
+
+
+    return '\n'.join(tag_txt)
+
+def get_all_siblings_tag_txt(html, tag='p'):
     """
     Parameters
     ------------
     html: Path()
         Path object pointing to hrml file
-    
+
+    tag: str
+        the name of the tag you want to find. Will find the first
+        element and all of its siblings.
+
     Returns
     ------------
-    ' '.join(p_txt): str
+    ' '.join(tag_txt): str
         returns string representing all paragraph txt
         in the html file
     """
-    p_txt = []
-    with open(html, 'r') as fh:
-        contents = fh.read()
-        soup = BeautifulSoup(contents, features="lxml")
-        next_sib = soup.find('p')
+    tag_txt = []
+    soup = get_soup(html)
+    next_sib = soup.find(tag)
 
-        while True:
-            try:
-                next_sib = next_sib.next_sibling
-            except AttributeError: #no more siblings
-                break
+    while True:
+        try:
+            next_sib = next_sib.next_sibling
+        except AttributeError: # no more siblings
+            break
 
-            try:
-                name = next_sib.name
-            except AttributeError:
-                name = ""
-            
-            if name == "p":
-                text = next_sib.text
-                p_txt.append(text)
+        # I think this could be improved modeling the
+        # function above...I believe this is a Navigatable
+        # string
 
-    return '\n'.join(p_txt)
+        try:
+            name = next_sib.name
+        except AttributeError:
+            name = ""
+        
+        if name == tag:
+            text = next_sib.text
+            tag_txt.append(text)
+
+    return '\n'.join(tag_txt)
 
 def main(html_dir, txt_dir):
     """
@@ -55,11 +100,16 @@ def main(html_dir, txt_dir):
     """     
     htmls = [x for x in Path(html_dir).glob('**/*') if x.is_file()]
     for html in htmls:
+        print(html)
+        print('\n'*5)
         txt_path = Path(txt_dir) / Path(Path(html).stem + '.txt')
-        p_txt = get_p_txt(html)
+        ul = get_descendants_of_tag_txt(html)
+        #tag_txt = get_all_siblings_tag_txt(html)
+        
+        #print(tag_txt)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
 
 
 
