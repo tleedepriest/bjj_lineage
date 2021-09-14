f5fd406c61c654de0638cfe32f91985912599199
Travis <tleedepriest@gmail.com>
2021-09-13 20:58:54 -0400

    commented out part where entities list was created and deduped. We only needed to perform this function once in order to find the duplicate values. Added in functions that read in said duplicate values in a manual file and dedupe the lineage list.

--- scripts/extract/extract_from_bjj_hero_p_txt.py
+++ scripts/extract/extract_from_bjj_hero_p_txt.py
@@ -11,12 +11,53 @@ utils_abs_dir_path = Path(
         Path(__file__).parent.resolve().parent) / Path('utils')
 sys.path.insert(0, str(utils_abs_dir_path))
 
-from file_utils import get_file_list, get_path_txt
+from file_utils import get_file_list, get_path_txt, get_path_lines
+
+def remove_xao(entity):
+    return entity.replace(u'\xa0', u' ')
+
+def clean_entity(entity):
+    # remove ( and spaces at beginning
+    if not entity[0].isalpha():
+        entity = entity[1:]
+        entity = entity.lstrip()
+    return entity
 
 def strip_accents(s):
        return ''.join(c for c in unicodedata.normalize('NFD', s)
                                  if unicodedata.category(c) != 'Mn')
 
+def invert_mapping(mapping):
+    inverted_mapping = {}
+    for key, value in mapping.items():
+        for ent in value:
+            inverted_mapping[ent] = key
+    return inverted_mapping
+
+
+def get_dedupe_mapping(mapping_lines):
+    """
+    Parameters
+    -------------
+    mapping_string: str
+        string containing contents to produce dedupe mapping dict
+    """
+    # split on newline, if two blank values occur
+    # in a row, then this means that the next value
+    # is a key
+    mapping = {}
+    key = mapping_lines[0]
+    mapping[key] = []
+    for num, line in enumerate(mapping_lines[1:]):
+        # need to shift num over since shifting mapping lins over
+        num+=1
+        if line == "":
+            key = mapping_lines[num+1]
+            mapping[key] = []
+        else:
+            mapping[key].append(line)
+    return mapping
+        
 def extract_lineage(txt):
     """
     Parameters
@@ -38,12 +79,15 @@ def extract_lineage(txt):
         return match.group(1)
     return None
 
-def main(txt_dir, outpath):
+def main(txt_dir, dedupe_mapping_path):
     """
     Parameters
     -------------
     txt_dir: str
         the directory containing all the txt files
+
+    dedupe_mapping_path: str
+        the path to the txt file containing the dedupe mapping
 
     outpath: str
         the path to the output csv
@@ -56,28 +100,34 @@ def main(txt_dir, outpath):
     ---------------
     produces csv of extracted contents from txt files
     """
+    # each chunk of entities seperated by newline
+    mapping_lines = get_path_lines(Path(dedupe_mapping_path))
+    mapping_lines = [remove_xao(ent) for ent in mapping_lines]
+    mapping = get_dedupe_mapping(mapping_lines)
+    inverted_mapping = invert_mapping(mapping)
+    print(inverted_mapping)
     txt_files = get_file_list(txt_dir)
     # make a list of unique entities so that we can analyze and perform some
     # manual deduplication
-    entities = []
+    # entities = []
+
     for txt_file in txt_files:
         txt = get_path_txt(txt_file)
         lin = extract_lineage(txt)
         if lin is not None:
             # child of parent followed by > symbol
             lin_path = lin.split(">")
+            clean_lin_path = []
             for entity in lin_path:
                 entity = entity.lstrip().rstrip()
                 entity = strip_accents(entity)
-                if not entity[0].isalpha():
-                    entity = entity[1:]
-                    entity = entity.lstrip()
-                entities.append(entity)
-    entities_deduped = list(set(entities))
-    entities_deduped.sort()
-    for ent in entities_deduped:
-        print(ent)
-    pd.DataFrame({"entity": entities_deduped}).to_csv(outpath, index=False)
+                entity = clean_entity(entity)
+                entity = remove_xao(entity)
+                if entity in inverted_mapping.keys():
+                    entity = inverted_mapping[entity]
+                clean_lin_path.append(entity)
+            print(clean_lin_path)
+    #pd.DataFrame({"entity": entities_deduped}).to_csv(outpath, index=False)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
