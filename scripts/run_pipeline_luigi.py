5fde4a2300101f2c2070898eb4f39826099c6f50
Travis <tleedepriest@gmail.com>
2021-09-14 22:29:16 -0400

    adding the outline for the next several tasks in the pipeline

--- scripts/run_pipeline_luigi.py
+++ scripts/run_pipeline_luigi.py
@@ -10,7 +10,9 @@ import scrapers.get_fighters_html
 
 # takes in Task class
 class GenerateFighterLinksCSV(luigi.Task):
-
+    """
+    visits bjjheroes and generates a csv of fighters.
+    """
     # define what is being made
     def output(self):
         return luigi.LocalTarget('generated_data/bjj_fighter_links.csv')
@@ -23,7 +25,10 @@ class GenerateFighterLinksCSV(luigi.Task):
 # different than above, because this will have a
 # dependency unlike Generate Words
 class RequestSaveHTML(luigi.Task):
-
+    """
+    reads csv of fighters and writes out html files of each fighter.
+    writes out marker file to indicate it is finished with task
+    """
     def requires(self):
         return GenerateFighterLinksCSV()
 
@@ -35,6 +40,19 @@ class RequestSaveHTML(luigi.Task):
                             self.input().path,
                             Path('generated_data/htmls')
                             )
+
+# extract all the ptags from the html and write them to a seperate txt file
+class TransformHTMLToPTags():
+
+#extract the lineage from the text file, perform preprocessing on tags
+class ExtractLineageFromPTags():
+
+# load lineage into database using clean_lineage_paths.csv
+# create entity-relation db Here
+class LoadLineageIntoDataBase():
+
+
+
  
 if __name__ == "__main__":
     luigi.run()
