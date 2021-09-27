37fea66c5ac46c11fd5a381e2cd972e0a1639eb8
Travis <tleedepriest@gmail.com>
2021-09-26 21:23:22 -0400

    added more steps to pipeline

--- scripts/run_pipeline_luigi.py
+++ scripts/run_pipeline_luigi.py
@@ -11,6 +11,7 @@ import scrapers.get_fighters_html
 
 import transform.transform_htmls_to_txt_p
 import extract.extract_from_bjj_hero_p_txt
+import transform.transform_clean_lineage_paths_csv
 
 class GenerateFighterLinksCSV(luigi.Task):
     """
@@ -69,12 +70,45 @@ class ExtractLineageFromPTags(luigi.Task):
     def run(self):
         extract.extract_from_bjj_hero_p_txt.main(
                 Path('transformed_data/txt_section/p_tags'),
+                Path('manual_data/deduplication.txt'),
+                self.output().path)
+
+class TransformLineagePathsToParentChild(luigi.Task):
+    def requires(self):
+        return ExtractLineageFromPTags()
+    
+    def output(self):
+        return luigi.LocalTarget('transformed_data/entity_parent_lineage_paths.csv')
+
+    def run(self):
+        transform.transform_clean_lineage_paths_csv.main(
+                self.input().path,
                 self.output().path)
 
 # load lineage into database using clean_lineage_paths.csv
 # create entity-relation db Here
 class LoadLineageIntoDataBase():
     pass
+
+#https://github.com/spotify/luigi/issues/595
+class ForceableTask(luigi.Task):
+    force = luigi.BoolParameter(significant=False, default=True)
+    force_upstream = luigi.BoolParameter(significant=False, default=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.force_upstream is True:
+            self.force = True
+        if self.force is True:
+            done = False
+            tasks = [self]
+            while not done:
+                outputs = luigi.task.flatten(tasks[0].output())
+                print(outputs)
+                if self.force_upstream is True: tasks+=luigi.task.flatten(tasks[0].requires())
+                tasks.pop(0)
+                if len(tasks) == 0:
+                    done = True
 
 if __name__ == "__main__":
     luigi.run()
