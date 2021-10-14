0758c3b77d0b37fed88fea00e600f194daa2bbdf
Travis <tleedepriest@gmail.com>
2021-10-13 21:05:07 -0400

    After revisiting, figured out how to implement the Forceable Task Class....pass this as a child class to the last task in the pipeline, then pass parameter in command line --force-upstream true to delete all upstream tasks. This will cause Luigi to rerun pipeline. Only files that do not exist in the folder will be scrapedgit status

--- scripts/run_pipeline_luigi.py
+++ scripts/run_pipeline_luigi.py
@@ -2,9 +2,10 @@
 Writing the script from the Luigi Documentation to build some
 muscle memory and test
 """
-import luigi
+import os
 import sys
 from pathlib import Path
+import luigi
 
 import scrapers.get_fighter_links_from_bjj_heros
 import scrapers.get_fighters_html
@@ -12,6 +13,40 @@ import scrapers.get_fighters_html
 import transform.transform_htmls_to_txt_p
 import extract.extract_from_bjj_hero_p_txt
 import transform.transform_clean_lineage_paths_csv
+
+#https://stackoverflow.com/questions/59842469/luigi-is-there-a-way-to-pass-false-to-a-bool-parameter-from-the-command-line
+#https://github.com/spotify/luigi/issues/595
+#https://stackoverflow.com/questions/34613296/how-to-reset-luigi-task-status
+class ForceableTask(luigi.Task):
+    force = luigi.BoolParameter(
+            significant=False, 
+            default=False,
+            parsing=luigi.BoolParameter.EXPLICIT_PARSING)
+
+    force_upstream = luigi.BoolParameter(
+            significant=False, 
+            default=False,
+            parsing=luigi.BoolParameter.EXPLICIT_PARSING)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.force_upstream is True:
+            self.force = True
+        if self.force is True:
+            done = False
+            tasks = [self]
+            while not done:
+                outputs = luigi.task.flatten(tasks[0].output())
+                #os.remove(out.path) for out in outputs if out.exists()]
+                for out in outputs:
+                    if out.exists():
+                        print(f"Removing {out.path}!")
+                        os.remove(out.path)
+                if self.force_upstream is True: 
+                    tasks+=luigi.task.flatten(tasks[0].requires())
+                tasks.pop(0)
+                if len(tasks) == 0:
+                    done = True
 
 class GenerateFighterLinksCSV(luigi.Task):
     """
@@ -73,7 +108,7 @@ class ExtractLineageFromPTags(luigi.Task):
                 Path('manual_data/deduplication.txt'),
                 self.output().path)
 
-class TransformLineagePathsToParentChild(luigi.Task):
+class TransformLineagePathsToParentChild(ForceableTask):
     def requires(self):
         return ExtractLineageFromPTags()
     
@@ -90,25 +125,6 @@ class TransformLineagePathsToParentChild(luigi.Task):
 class LoadLineageIntoDataBase():
     pass
 
-#https://github.com/spotify/luigi/issues/595
-class ForceableTask(luigi.Task):
-    force = luigi.BoolParameter(significant=False, default=True)
-    force_upstream = luigi.BoolParameter(significant=False, default=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.force_upstream is True:
-            self.force = True
-        if self.force is True:
-            done = False
-            tasks = [self]
-            while not done:
-                outputs = luigi.task.flatten(tasks[0].output())
-                print(outputs)
-                if self.force_upstream is True: tasks+=luigi.task.flatten(tasks[0].requires())
-                tasks.pop(0)
-                if len(tasks) == 0:
-                    done = True
 
 if __name__ == "__main__":
     luigi.run()
