6f5e64baa2da3ad7111154d630c68c64713d6ccd
Travis <tleedepriest@gmail.com>
2021-12-19 22:11:31 -0500

    adding moved scripts

---
+++ scripts/simple_txt_search.py
@@ -0,0 +1,56 @@
+"""
+This script will perform a simple text search and create visualization to add
+in the extraction of key fields from the text.
+"""
+import sys
+import pandas as pd
+from collections import defaultdict
+from pathlib import Path
+utils_abs_dir_path = Path(
+        Path(__file__).parent.resolve().parent) / Path('utils')
+sys.path.insert(0, str(utils_abs_dir_path))
+
+from file_utils import get_file_list, get_path_txt
+
+def main(txt_dir, out_csv_path):
+    """
+    txt_dir: str
+        the path to the directory you want to search
+
+    out_csv_path: str
+        the path of the csv file to produce
+    """
+    # include common word for sanity check
+    # and to make sure same as number of files
+    words_to_count = [
+            "Lineage 1:", 
+            "Lineage:", 
+            "Lineage :", 
+            "Linage:",
+            "Full Name:",
+            "Nickname:",
+            "Full Name :",
+            "Nickname :",
+            "and",
+            "Lineage 2:",
+            "Lineage 3:",
+            "Lineage 4:",
+            "Home  BJJ Fighter DatabaseCategory", # appears before desired txt
+            "Next Article"] # appears after desired txt
+
+    txt_files = get_file_list(txt_dir)
+    tally_occurence = defaultdict(int)
+
+    for txt_file in txt_files:
+        contents = get_path_txt(txt_file)
+        for word in words_to_count:
+            if word in contents:
+                tally_occurence[word]+=1
+            else:
+                tally_occurence[word]+=0
+    df = pd.DataFrame(tally_occurence.items(), 
+            columns=["word", "count_across_corpus"])
+    df.to_csv(out_csv_path, index=False)
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
---
+++ scripts/transform_htmls_to_txt.py
@@ -0,0 +1,58 @@
+"""
+Convert html2text so I can run some text analytics to understand the data
+and structure of each webpage. Running into edge cases while trying to populate
+database.
+"""
+import sys
+from pathlib import Path
+from bs4 import BeautifulSoup
+def get_soup_from_static_html(html):
+    """
+    Parameters
+    ----------------
+    html: str/path obj
+        the path to the html file
+
+    Returns
+    -----------------
+    soup: bs4 Soup obj
+    """
+    with open(html, 'r') as fh:
+        soup = BeautifulSoup(fh, 'html.parser')
+        #contents = fh.read()
+        #soup = BeautifulSoup(contents, features='lxml')
+    return soup
+
+def main(html_dir, txt_dir, marker_file):
+    """
+    Parameters
+    --------------
+    html_dir: str
+        contains htmls extracted from bjj_heros -> generated_data/htmls
+
+    txt_dir: str
+        script transform htmls to txt files -> transformed_data/html_to_txt
+
+    Returns
+    ----------------
+    None
+
+    Side Effects
+    ---------------
+    Writes text file for each html file to txt_dir
+    """
+    htmls = [x for x in Path(html_dir).glob('**/*') if x.is_file()]
+    for html in htmls:
+            txt_path = Path(txt_dir) / Path(Path(html).stem + '.txt')
+            soup = get_soup_from_static_html(html)
+            text = soup.get_text()
+            if not txt_path.is_file():
+                with open(txt_path, 'w') as fh:
+                    fh.write(text)
+
+    # write marker file as dependency for Luigi Pipeline
+    with open(marker_file, "w") as fh:
+        fh.write("")
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
