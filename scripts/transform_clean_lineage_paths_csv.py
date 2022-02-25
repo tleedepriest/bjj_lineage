bd0ed9f9aeb6422dc4843e6cb4e10c95d2b57e2a
Travis <tleedepriest@gmail.com>
2022-02-25 09:36:15 -0500

    fixed no path showing up in visualization. changed no_path to no path

--- scripts/transform_clean_lineage_paths_csv.py
+++ scripts/transform_clean_lineage_paths_csv.py
@@ -28,7 +28,7 @@ def get_ent_id_mapping(df_with_lin):
             df_with_lin["lineage"].astype(str).to_list()
             ):
         # these will go in another DB later on
-        if lineage == "no_path":
+        if lineage == "no path":
             pass
         else:
             # parent, child, grandchild, ..
@@ -40,7 +40,7 @@ def get_ent_id_mapping(df_with_lin):
                 else:
                     prev_ind = ind-1
                     prev_ent = ent_list[prev_ind]
-                    
+
                 if ent not in ent_id_mapping:
                     ent_id_mapping[ent] = unique_id
                     unique_id+=1
@@ -49,10 +49,10 @@ def get_ent_id_mapping(df_with_lin):
 
                 if ent_id_mapping[ent] not in entity_ids:
                     entity_ids.append(ent_id_mapping[ent])
-                
+
                 if ent_id_mapping[prev_ind] not in parent_ids:
                     parent_ids.append(ent_id_mapping[prev_ind])
-                
+
                 if entity not in entities:
                     entities.append
     entity_df = pd.DataFrame({
@@ -72,7 +72,7 @@ def main(in_path, out_path):
     """
     df_with_lin = pd.read_csv(in_path)
     #ent_id_mapping = get_ent_id_mapping(df_with_lin=clean_lin_paths)
-    
+
     ent_id_mapping = {}
     unique_id = 0
     entity_ids = []
@@ -83,7 +83,7 @@ def main(in_path, out_path):
             df_with_lin["lineage"].astype(str).to_list()
             ):
         # these will go in another DB later on
-        if lineage == "no_path":
+        if lineage == "no path":
             pass
         else:
             # parent, child, grandchild, ..
@@ -91,13 +91,13 @@ def main(in_path, out_path):
             for ind, ent in enumerate(ent_list):
                 if ind == 0:
                     prev_ind = None # root
-                    prev_ent = None  
+                    prev_ent = None
                 else:
-                    prev_ind = ind-1 
+                    prev_ind = ind-1
                     prev_ent = ent_list[prev_ind] # this is the parent id
-                    
+
                 if ent not in ent_id_mapping:
-                    ent_id_mapping[ent] = unique_id 
+                    ent_id_mapping[ent] = unique_id
                     entity_ids.append(ent_id_mapping[ent])
                     entities.append(ent)
                     unique_id+=1
@@ -109,12 +109,12 @@ def main(in_path, out_path):
                         parent_ids.append(ent_id_mapping[prev_ent])
                 else:
                     pass
-                
+
     entity_df = pd.DataFrame({
         "entity_id": entity_ids,
         "parent_id": parent_ids,
         "entity":entities})
-    
+
     entity_df.to_csv(out_path, index=False)
 
 if __name__ == "__main__":
